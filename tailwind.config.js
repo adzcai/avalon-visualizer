037d8b20b772b6c364045685d3ce060200b74d64
Alexander Cai <alexandercai@ISTs-MacBook-Pro.local>
2020-08-26 21:39:55 -0600

    fix missing tailwind classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ module.exports = {
   purge: {
     content: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
     options: {
-      whitelist: ['bg-blue-800', 'bg-red-200', 'bg-red-800', 'bg-green-200', 'bg-green-800'],
+      whitelist: ['bg-blue-600', 'bg-blue-800', 'bg-red-200', 'bg-red-600', 'bg-red-800', 'bg-green-200', 'bg-green-800'],
     },
   },
   theme: {
